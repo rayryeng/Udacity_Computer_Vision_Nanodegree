bd2d33932177cd7557a843c24635acc39be8ff81
Ray Phan <rphan@ryerson.ca>
2020-06-10 07:43:52 -0400

    Project 1: Complete

--- Facial-Keypoint-Detection-P1/models.py
+++ Facial-Keypoint-Detection-P1/models.py
@@ -1,37 +1,80 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
+#from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
-import torch.nn.init as I
+#import torch.nn.init as I
 
 
 class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        
+
         ## TODO: Define all the layers of this CNN, the only requirements are:
         ## 1. This network takes in a square (same width and height), grayscale image as input
         ## 2. It ends with a linear layer that represents the keypoints
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        
+
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        
+        # Architecture - (CONV-RELU-POOL) - (CONV-RELU-POOL) - (CONV-RELU-POOL) - (CONV-RELU-POOL) - (FLATTEN) - (DROPOUT - FC) - (DROPOUT - FC) - (DROPOUT - FC)
+        # Width:  224     220       110      106        53       49        24       20        10      256 x 10 x 10         512              256              136
+        # Channels:        32                 64                128                 256
+
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+        self.conv4 = nn.Conv2d(128, 256, 5)
 
-        
+        self.pool = nn.MaxPool2d((2, 2))
+
+        self.dropout1 = nn.Dropout(0.3)
+        self.fc1 = nn.Linear(256 * 10 * 10, 1024)
+
+        self.dropout2 = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(1024, 1024)
+
+        self.dropout3 = nn.Dropout(0.3)
+        self.fc3 = nn.Linear(1024, 136)
+
+
+
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = self.pool(x)
+
+        x = x.view(x.size(0), -1)
+
+        x = self.dropout1(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+
+        x = self.dropout2(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+
+        x = self.dropout3(x)
+        x = self.fc3(x)                
         # a modified x, having gone through all the layers of your model, should be returned
         return x
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Udacity_Computer_Vision_Nanodegree
-My work from the Computer Vision Nanodegree Program
+
+This repo contains my work for the Udacity Computer Vision Nanodegree Program.  Each directory contains a project required for completion and also contains instructions on what is expected to complete each project successfully.  Also note that the code uses PyTorch 1.x and not 0.x so some slight changes were made to ensure the code runs.
+
+## Project #1 - Facial Keypoints Detection
+
+The goal of this project is to detect where the facial keypoints are on a face image by designing an appropriate deep neural network to directly regress their locations on the face.  The relevant models, notebooks and results can be found in `Facial-Keypoint-Detection-P1`.   The trained model for this project has been committed via Git LFS so this will need to be installed if you want to grab the model.
+
+Please visit <https://github.com/git-lfs/git-lfs/wiki/Installation> to install Git LFS. Once you do, after you clone this repo, please use the following to download the weights.
+
+```sh
+$ cd <path to this repo>
+$ git lfs fetch
+```
